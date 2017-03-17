d7e2c72d9d1adde2161aa199fee0950e7b9d4377
Todd Bennett <todd.m.bennett@gmail.com>
2017-03-17 16:19:38 +0900

    Practice controller setup, environment, packages

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -6,5 +6,39 @@ use Illuminate\Http\Request;
 
 class PracticeController extends Controller
 {
-    //
+
+    /**
+    *
+    */
+    public function practice2() {
+        dump(config('app'));
+    }
+
+    /**
+    *
+    */
+    public function practice1() {
+      dump('This is the first example.');
+    }
+
+    /**
+    * ANY (GET/POST/PUT/DELETE)
+    * /practice/{n?}
+    *
+    * This method accepts all requests to /practice/ and
+    * invokes the appropriate method.
+    *
+    * http://localhost/practice/1 => Invokes practice1
+    * http://localhost/practice/5 => Invokes practice5
+    * http://localhost/practice/999 => Invokes practice999
+    */
+    public function index($n) {
+
+        $method = 'practice'.$n;
+
+        if(method_exists($this, $method))
+            return $this->method();
+        else
+            dd("Practice route [{$n}] not defined");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,14 @@ Route::get('/books', 'BookController@index');
 
 Route::get('/books/{title?}', 'BookController@view');
 
+if(config('app.env') == 'local') {
+    Route::get('/logs', function() { });
+}
+
+if(config('app.env') == 'local') {
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
+
+Route::any('/practice/{n?}', 'PracticeController@index');
+
 Route::get('/', 'WelcomeController');
